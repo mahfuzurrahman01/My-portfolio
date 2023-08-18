807ecda955886957637780a66a40e416a55d8157
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-08-18 20:40:08 +0600

    modified experience section first

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,18 +13,59 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init();
 
 const About = () => {
-    const [state, setState] = useState(false)
+    const [state, setState] = useState(true)
     return (
         <div id='about' className='md:pt-10 pt-5'>
             <Title name={'About Me'} />
 
             <div className='text-center flex justify-center items-center md:gap-10 gap-5 text'>
+                <p onClick={() => setState(true)} className={`flex justify-center items-center gap-2 ${state ? "border-b-2 border-gray-500" : "border-b-2 border-transparent"} text-gray-700 px-5  cursor-pointer duration-300 hover:bg-gray-300 mt-3 py-2 rounded-lg`}>Experience <GrUserExpert /> </p>
                 <p onClick={() => setState(false)} className={`flex justify-center items-center gap-2 ${!state ? "border-b-2 border-gray-500" : "border-b-2 border-transparent"} text-gray-700 px-5  cursor-pointer duration-300 hover:bg-gray-300 mt-3 py-2 rounded-lg`}>Certificate <GrCertificate /> </p>
 
-                <p onClick={() => setState(true)} className={`flex justify-center items-center gap-2 ${state ? "border-b-2 border-gray-500" : "border-b-2 border-transparent"} text-gray-700 px-5  cursor-pointer duration-300 hover:bg-gray-300 mt-3 py-2 rounded-lg`}>Experience <GrUserExpert /> </p>
             </div>
 
+            
+
+            {/*====================== this section is for experience ========================= */}
+
             {
+                state && <section className=" text-gray-700 md:mt-5 mt-2" data-aos="zoom-in" data-aos-duration="1500">
+                    <div className="g:px-5 px-1 py-12 mx-auto lg:w-[90%] w-[96%]">
+                        <div className="grid gap-4 lg:mx-4 mx-1 sm:grid-cols-12">
+                            <div className="col-span-12 sm:col-span-3">
+                                <div className="text-center sm:text-left md:mb-14 mb-7 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-slate-500">
+                                    <h3 className="text-3xl font-semibold">Experiences</h3>
+                                    <span className="text-sm font-bold tracking-wider uppercase text-gray-400">Job / Freelancing</span>
+                                </div>
+                            </div>
+                            <div className="relative col-span-12 md:px-4 px-2 space-y-6 sm:col-span-9">
+                                <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700">
+                                    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-500">
+                                        <div className='flex justify-between items-center'>
+                                            <h3 className="text-xl font-semibold tracking-wide">Front-end Developer at Zeigen</h3>
+                                            <img src={zeigenLogo} alt="meta-logo" className='w-20 h-7 mr-1' />
+                                        </div>
+                                        <time className="text-xs tracking-wide uppercase text-gray-400">Jan 2023 - present</time>
+                                        <p className="mt-3 md:text-lg text-sm md:text-left text-justify">I'm building the Zeigen frontend.Zeigen is a health care service providing website.It is designed to promote and sell healthcare services provided by doctors and other medical professionals, such as consultations, appointments, and test lists. The website will likely include information about the services offered, pricing, and details about the healthcare providers. Users can book appointments and take advantage of the healthcare services offered through the website</p>
+                                    </div>
+                                    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-500">
+                                        <div className='flex justify-between items-center'>
+                                            <h3 className="text-xl font-semibold tracking-wide">ReactJs Developer at CalciteX</h3>
+                                            <img src={calcitelogo} alt="meta-logo" className='w-16 h-5 mr-1' />
+                                        </div>
+                                        <time className="text-xs tracking-wide uppercase text-gray-400">Dec 2022 - Jan 2023</time>
+                                        <p className="mt-3 md:text-lg text-sm md:text-left text-justify">I've done my internship from calciteX as a reactJs developer.It was 2 months of internship program.Where my main task was to build the UI with React and make reusable components.In this period I've build 3 CRM website UI with reactJs and work on some existing projects also.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            }
+
+ {/* ============== this section is for Certificate ================  */}
+ {
                 !state && <section data-aos="zoom-in" data-aos-duration="1500" className="text-gray-700 md:mt-5 mt-2">
                     <div className="lg:px-5 px-1 py-12 mx-auto lg:w-[90%] w-[96%]">
                         <div className="grid gap-4 lg:mx-4 mx-1 sm:grid-cols-12">
@@ -71,43 +112,6 @@ const About = () => {
                 </section>
             }
 
-            {/*====================== this section is for experience ========================= */}
-
-            {
-                state && <section className=" text-gray-700 md:mt-5 mt-2" data-aos="zoom-in" data-aos-duration="1500">
-                    <div className="g:px-5 px-1 py-12 mx-auto lg:w-[90%] w-[96%]">
-                        <div className="grid gap-4 lg:mx-4 mx-1 sm:grid-cols-12">
-                            <div className="col-span-12 sm:col-span-3">
-                                <div className="text-center sm:text-left md:mb-14 mb-7 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-slate-500">
-                                    <h3 className="text-3xl font-semibold">Experiences</h3>
-                                    <span className="text-sm font-bold tracking-wider uppercase text-gray-400">Job / Freelancing</span>
-                                </div>
-                            </div>
-                            <div className="relative col-span-12 md:px-4 px-2 space-y-6 sm:col-span-9">
-                                <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700">
-                                    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-500">
-                                        <div className='flex justify-between items-center'>
-                                            <h3 className="text-xl font-semibold tracking-wide">Front-end Developer at Zeigen</h3>
-                                            <img src={zeigenLogo} alt="meta-logo" className='w-20 h-7 mr-1' />
-                                        </div>
-                                        <time className="text-xs tracking-wide uppercase text-gray-400">Jan 2023 - present</time>
-                                        <p className="mt-3 md:text-lg text-sm md:text-left text-justify">I'm building the Zeigen frontend.Zeigen is a health care service providing website.It is designed to promote and sell healthcare services provided by doctors and other medical professionals, such as consultations, appointments, and test lists. The website will likely include information about the services offered, pricing, and details about the healthcare providers. Users can book appointments and take advantage of the healthcare services offered through the website</p>
-                                    </div>
-                                    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-500">
-                                        <div className='flex justify-between items-center'>
-                                            <h3 className="text-xl font-semibold tracking-wide">ReactJs Developer at CalciteX</h3>
-                                            <img src={calcitelogo} alt="meta-logo" className='w-16 h-5 mr-1' />
-                                        </div>
-                                        <time className="text-xs tracking-wide uppercase text-gray-400">Dec 2022 - Jan 2023</time>
-                                        <p className="mt-3 md:text-lg text-sm md:text-left text-justify">I've done my internship from calciteX as a reactJs developer.It was 2 months of internship program.Where my main task was to build the UI with React and make reusable components.In this period I've build 3 CRM website UI with reactJs and work on some existing projects also.</p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            }
         </div>
     );
 };
